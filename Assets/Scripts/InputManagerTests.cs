832570cb7401bd42ab43fdf6e2b8a7489dd99245
MartiGuillot <MartiGuillot@users.noreply.github.com>
2019-11-16 14:53:33 +0100

    Controls

--- Assets/Scripts/InputManagerTests.cs
+++ Assets/Scripts/InputManagerTests.cs
@@ -13,10 +13,30 @@ public class InputManagerTests : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.GetJoystickNames().Length);
-        if (Input.GetButton("Submit"))
+        //Debug.Log(Input.GetJoystickNames().Length);
+        //if (Input.GetButton("Submit"))
+        //{
+        //    Debug.Log("Submitting");
+        //}
+        //Debug.Log(Input.GetAxis("Joystick1Horizontal"));
+        //Debug.Log(Input.GetAxis("[1]"));
+        if (Input.GetAxis("Joystick1Horizontal") < -0.1f)
         {
-            Debug.Log("Submitting");
+            Debug.Log("Negative Joystick1Horizontal: " + Input.GetAxis("Joystick1Horizontal"));
         }
+        if (Input.GetAxis("Joystick2Horizontal") < -0.1f)
+        {
+            Debug.Log("Negative Joystick2Horizontal: " + Input.GetAxis("Joystick2Horizontal"));
+        }
+
+        if (Input.GetAxis("Joystick1Horizontal") > 0.1f)
+        {
+            Debug.Log("Positive Joystick1Horizontal: " + Input.GetAxis("Joystick1Horizontal"));
+        }
+        if (Input.GetAxis("Joystick2Horizontal") > 0.1f)
+        {
+            Debug.Log("Positive Joystick2Horizontal: " + Input.GetAxis("Joystick2Horizontal"));
+        }
+        //Debug.Log(Input.GetJoystickNames()[0]);
     }
 }
