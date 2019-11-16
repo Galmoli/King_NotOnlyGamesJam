6ea16306bbedda2ffdea3cfd43766af8d6f99778
Toni Galmés Oliver <tonigalmes98@gmail.com>
2019-11-16 11:19:59 +0100

    Spawner

---
+++ Assets/Scripts/CubeOrder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeOrder : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private bool onMove;
+
+    public bool instanced;
+
+    public CubeOrder[] cubeOrderVector;
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        instanced = true;
+    }
+
+    private void Update()
+    {
+        if (!onMove && AreAllCubesInstanced())
+        {
+            onMove = true;
+            StartCoroutine(Move());
+        }
+        if(transform.position.x >= 28.5) DisableCube();
+    }
+
+    private IEnumerator Move()
+    {
+        while (onMove)
+        {
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+            yield return null;
+        }
+    }
+
+    public void DisableCube()
+    {
+        onMove = false;
+        gameObject.SetActive(false);
+    }
+
+    private bool AreAllCubesInstanced() //Sees if all cubes are instantiated. It makes them start at the same time.
+    {
+        foreach (var c in cubeOrderVector)
+        {
+            if (!c.instanced) return false;
+        }
+        return true;
+    }
+}
--- Assets/Scripts/SpawnerController.cs
+++ Assets/Scripts/SpawnerController.cs
@@ -4,15 +4,114 @@ using UnityEngine;
 
 public class SpawnerController : MonoBehaviour
 {
+    [SerializeField] private int numOfOrderCubes = 2;
+    [SerializeField] private float initialSpawnTime = 4;
+    [SerializeField] private float minSpawnTime = 2;
+    [SerializeField] private float spawnTimeDecrement = 0.1f;
+
+    //Prefabs
+    [SerializeField] private GameObject upArrow;
+    [SerializeField] private GameObject downArrow;
+    [SerializeField] private GameObject rightArrow;
+    [SerializeField] private GameObject leftArrow;
+    
+    //SpawnPositions
+    [SerializeField] private Transform[]  positionsVector;
+
+    private GameObject[] orderCubeVector;
+    
+    private float currentTime;
+    private float spawnTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        orderCubeVector = new GameObject[numOfOrderCubes];
+        spawnTime = initialSpawnTime;
+        currentTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentTime >= spawnTime)
+        {
+            GetNextCubeOrderVector();
+            SpawnOrder();
+            currentTime = 0;
+            if(spawnTime > minSpawnTime + spawnTimeDecrement) spawnTime -= spawnTimeDecrement;
+        }
+        currentTime += Time.deltaTime;
+    }
+
+    private void SpawnOrder()
+    {
+        foreach (var go in orderCubeVector)
+        {
+            go.SetActive(true);
+        }
+    }
+
+    private void GetNextCubeOrderVector()
+    {
+        GameObject[] vector = new GameObject[numOfOrderCubes];
+
+        for (int i = 0; i < orderCubeVector.Length; i++)
+        {
+            int randomN = Random.Range(0, 4);
+            switch (randomN)
+            {
+                case 0:
+                {
+                    vector[i] = Instantiate(upArrow, transform);
+                    break;
+                }
+                case 1:
+                {
+                    vector[i] = Instantiate(downArrow, transform);
+                    break;
+                }
+                case 2:
+                {
+                    vector[i] = Instantiate(rightArrow, transform);
+                    break;
+                }
+                case 3:
+                {
+                    vector[i] = Instantiate(leftArrow, transform);
+                    break;
+                }
+            }
+            vector[i].SetActive(false);
+        }
+
+        orderCubeVector = vector;
+        SetPositions();
+        foreach (var c in orderCubeVector)
+        {
+            c.GetComponent<CubeOrder>().cubeOrderVector = GetCubeOrderComponents();
+        }
+    }
+
+    private void SetPositions()
+    {
+        int k;
+        if (numOfOrderCubes == 2) k = 1;
+        else k = 0;
+        for (int i = k; i < orderCubeVector.Length; i++)
+        {
+            orderCubeVector[i].transform.position = positionsVector[i].position;
+        }
+    }
+
+    private CubeOrder[] GetCubeOrderComponents()
+    {
+        CubeOrder[] components = new CubeOrder[numOfOrderCubes];
+        for(int i = 0; i < orderCubeVector.Length; i++)
+        {
+            components[i] = orderCubeVector[i].GetComponent<CubeOrder>();
+        }
+
+        return components;
     }
 }
