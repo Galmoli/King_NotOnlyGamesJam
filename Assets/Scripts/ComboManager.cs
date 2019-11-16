bd024d14d38d19ca760bf4d0c424c86492a4d17f
MartiGuillot <MartiGuillot@users.noreply.github.com>
2019-11-16 22:28:24 +0100

    Escena environment
    He tocat la escena per incloure la highscore i posar música i el combo manager l'he millorat

--- Assets/Scripts/ComboManager.cs
+++ Assets/Scripts/ComboManager.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ComboManager : MonoBehaviour
 {
     //Gestio del combo
     private int comboRacha;
-    public string[] comboWords;
+    private int comboAnnouncers;
+    public Image[] comboWords;
 
     //Highscore
     public float highScore = 0;
-    public Text highScoreText;
+    public TextMeshProUGUI highScoreText;
+    public int highScoreMultiplier = 15;
 
     //Screenshake
     [SerializeField] private Transform cameraTransform;
@@ -23,6 +26,7 @@ public class ComboManager : MonoBehaviour
     private void Awake()
     {
         initialPosition = cameraTransform.localPosition;
+        highScoreText.text = highScore.ToString();
     }
 
     // Start is called before the first frame update
@@ -39,8 +43,9 @@ public class ComboManager : MonoBehaviour
     void Update()
     {
         //Aqui es podria augmentar el tema filtros per fer-ho mes epic quan portin una racha queflips
-
         //Failure
+
+
         if (initialShakeDuration > 0)
         {
             cameraTransform.localPosition = initialPosition + Random.insideUnitSphere * shakeMagnitude;
@@ -57,9 +62,34 @@ public class ComboManager : MonoBehaviour
     public void MoreCombos()
     {
         comboRacha++;
-        Debug.Log(comboRacha);
-        Debug.Log(comboWords[comboRacha]);
-        if (comboWords.Length - 1 < comboRacha) comboRacha--;
+        if (comboWords.Length - 1 > comboRacha) comboRacha = comboWords.Length - 1;
+        highScore += comboRacha * highScoreMultiplier;
+        highScoreText.text = highScore.ToString();
+        switch (comboRacha)
+        {
+            case 0:
+                break;
+            case 1:
+                comboWords[0].enabled = true;
+                break;
+            case 2:
+                comboWords[1].enabled = true;
+                break;
+            case 3:
+                comboWords[2].enabled = true;
+                break;
+            case 4:
+                comboWords[3].enabled = true;
+                break;
+            case 5:
+                comboWords[4].enabled = true;
+                break;
+            case 6:
+                comboWords[5].enabled = true;
+                break;
+            default:
+                break;
+        }
     }
 
     public void YouFailed()
