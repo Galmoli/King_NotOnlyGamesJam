450fe8bfb310c650492c819244ae55c8f303ead7
MartiGuillot <MartiGuillot@users.noreply.github.com>
2019-11-16 17:32:36 +0100

    MusicManager
    Loopeja correctament la música

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    [SerializeField] private AudioSource firstPlay;
+    [SerializeField] private AudioSource songLoop;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (firstPlay.isPlaying)
+        {
+
+        }
+        else if (!songLoop.isPlaying)
+        {
+            songLoop.Play();
+        }
     }
 }
