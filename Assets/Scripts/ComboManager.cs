316abad5e4e82bceed9414a7e22e4b76da11e809
MartiGuillot <MartiGuillot@users.noreply.github.com>
2019-11-16 19:11:02 +0100

    betterments in the combo manager

--- Assets/Scripts/ComboManager.cs
+++ Assets/Scripts/ComboManager.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ComboManager : MonoBehaviour
 {
-    public PlayerController pController;
-    public GameManager gManager;
-
     //Gestio del combo
     private int comboRacha;
     public string[] comboWords;
+
+    //Highscore
+    public float highScore = 0;
+    public Text highScoreText;
 
     //Screenshake
     [SerializeField] private Transform cameraTransform;
@@ -36,12 +38,13 @@ public class ComboManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Aqui es podria augmentar el tema filtros per fer-ho mes epic quan portin una racha queflips//Failure
+        //Aqui es podria augmentar el tema filtros per fer-ho mes epic quan portin una racha queflips
 
         //Failure
         if (initialShakeDuration > 0)
         {
             cameraTransform.localPosition = initialPosition + Random.insideUnitSphere * shakeMagnitude;
+            Debug.Log(transform.localPosition);
             initialShakeDuration -= Time.deltaTime * dampingSpeed;
         }
         else
@@ -54,11 +57,15 @@ public class ComboManager : MonoBehaviour
     public void MoreCombos()
     {
         comboRacha++;
+        Debug.Log(comboRacha);
+        Debug.Log(comboWords[comboRacha]);
+        if (comboWords.Length - 1 < comboRacha) comboRacha--;
     }
 
     public void YouFailed()
     {
         comboRacha = 0;
+        Debug.Log("Awful :(");
         initialShakeDuration = 0.25f;
     }
 }
