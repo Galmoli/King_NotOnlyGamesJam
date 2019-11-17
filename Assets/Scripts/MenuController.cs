82261c14941b72ad7347de68ff8e90a2889e1986
Toni Galmés Oliver <tonigalmes98@gmail.com>
2019-11-17 01:42:11 +0100

    Menu efects

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
     [SerializeField] private Animator NG_Anim;
     [SerializeField] private Animator Exit_Anim;
+    [SerializeField] private AudioClip menuSelected;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float changeSceneDelay = 0.6f;
 
     private bool newGameSelected;
     // Start is called before the first frame update
     void Start()
     {
-        
+        newGameSelected = true;
     }
 
     // Update is called once per frame
@@ -34,8 +39,24 @@ public class MenuController : MonoBehaviour
 
         if (hinput.gamepad[0].A.justPressed)
         {
-            if (newGameSelected) SceneManager.LoadScene(1);
+            if (newGameSelected)
+            {
+                audioSource.PlayOneShot(menuSelected);
+                StartCoroutine(ChangeScene());
+            }
             else Application.Quit();
         }
+    }
+
+    private IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(changeSceneDelay);
+        while (fadeImage.color.a < 1)
+        {
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, fadeImage.color.a + 0.8f * Time.deltaTime);
+            audioSource.volume -= 0.8f * Time.deltaTime;
+            yield return null;
+        }
+        SceneManager.LoadScene(1);
     }
 }
